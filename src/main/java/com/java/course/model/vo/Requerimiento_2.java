6085c9d4c255293991df076930df5c1b9b533f7e
Nimoscmp <nimos.cmp@gmail.com>
2021-08-20 18:02:43 -0500

    Build requirement 2 java bean

---
+++ src/main/java/com/java/course/model/vo/Requerimiento_2.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.java.course.model.vo;
+
+/**
+ *
+ * @author nicolas
+ */
+public class Requerimiento_2 {
+    private String proveedor;
+    private String constructora;
+    private String pagado;
+
+
+    public String getProveedor() {
+        return this.proveedor;
+    }
+
+    public void setProveedor(String proveedor) {
+        this.proveedor = proveedor;
+    }
+
+    public String getConstructora() {
+        return this.constructora;
+    }
+
+    public void setConstructora(String constructora) {
+        this.constructora = constructora;
+    }
+
+    public String getPagado() {
+        return this.pagado;
+    }
+
+    public void setPagado(String pagado) {
+        this.pagado = pagado;
+    }
+}
