e2f6fc0c84680027868a40a2b3b82a65accded04
Nimoscmp <nimos.cmp@gmail.com>
2021-08-20 23:12:56 -0500

    Modify GUI

--- src/main/java/com/java/course/view/GUIConstruction.java
+++ src/main/java/com/java/course/view/GUIConstruction.java
@@ -32,11 +32,17 @@ public class GUIConstruction extends javax.swing.JFrame {
         SearchButton = new javax.swing.JButton();
         Separator = new javax.swing.JSeparator();
         OptionsTab = new javax.swing.JTabbedPane();
-        Req3 = new javax.swing.JPanel();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        jTextField1 = new javax.swing.JTextField();
         Req1 = new javax.swing.JPanel();
+        Title = new javax.swing.JLabel();
+        jSplitPane1 = new javax.swing.JSplitPane();
+        jPanel1 = new javax.swing.JPanel();
+        jPanel2 = new javax.swing.JPanel();
+        ScrollQuery1 = new javax.swing.JScrollPane();
+        TableQuery1 = new javax.swing.JTable();
         Req2 = new javax.swing.JPanel();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        Req3 = new javax.swing.JPanel();
+        jScrollPane3 = new javax.swing.JScrollPane();
         QueryOptions = new javax.swing.JComboBox<>();
         jButton2 = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
@@ -49,7 +55,8 @@ public class GUIConstruction extends javax.swing.JFrame {
         setSize(new java.awt.Dimension(550, 300));
 
         SearchButton.setBackground(new java.awt.Color(254, 254, 254));
-        SearchButton.setForeground(new java.awt.Color(254, 13, 13));
+        SearchButton.setFont(new java.awt.Font("Ubuntu", 1, 15)); // NOI18N
+        SearchButton.setForeground(new java.awt.Color(69, 126, 47));
         SearchButton.setText("Buscar");
         SearchButton.setMargin(new java.awt.Insets(1, 3, 1, 3));
         SearchButton.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -60,52 +67,158 @@ public class GUIConstruction extends javax.swing.JFrame {
 
         OptionsTab.setToolTipText("");
 
-        jTextField1.setText("jTextField1");
-        jScrollPane1.setViewportView(jTextField1);
+        Req1.setLayout(new java.awt.BorderLayout());
 
-        javax.swing.GroupLayout Req3Layout = new javax.swing.GroupLayout(Req3);
-        Req3.setLayout(Req3Layout);
-        Req3Layout.setHorizontalGroup(
-            Req3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 637, Short.MAX_VALUE)
-        );
-        Req3Layout.setVerticalGroup(
-            Req3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 187, Short.MAX_VALUE)
-        );
+        Title.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        Title.setLabelFor(Req1);
+        Title.setText("Proyectos");
+        Title.setMaximumSize(new java.awt.Dimension(70, 25));
+        Title.setMinimumSize(new java.awt.Dimension(70, 25));
+        Req1.add(Title, java.awt.BorderLayout.PAGE_START);
 
-        OptionsTab.addTab("Requerimiento 1", Req3);
+        jSplitPane1.setDividerLocation(40);
+        jSplitPane1.setOrientation(javax.swing.JSplitPane.VERTICAL_SPLIT);
 
-        javax.swing.GroupLayout Req1Layout = new javax.swing.GroupLayout(Req1);
-        Req1.setLayout(Req1Layout);
-        Req1Layout.setHorizontalGroup(
-            Req1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 637, Short.MAX_VALUE)
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 656, Short.MAX_VALUE)
         );
-        Req1Layout.setVerticalGroup(
-            Req1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 187, Short.MAX_VALUE)
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
         );
 
-        OptionsTab.addTab("Requerimiento 2", Req1);
+        jSplitPane1.setTopComponent(jPanel1);
 
-        Req2.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
-        Req2.setAlignmentX(0.2F);
-        Req2.setAlignmentY(0.2F);
-        Req2.setPreferredSize(new java.awt.Dimension(500, 180));
+        jPanel2.setLayout(new java.awt.BorderLayout());
+
+        TableQuery1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null}
+            },
+            new String [] {
+                "Constructora", "Ciudad", "Banco", "Porcentaje", "Clasificación", "Fecha"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.Double.class, java.lang.String.class, java.lang.String.class
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+        });
+        ScrollQuery1.setViewportView(TableQuery1);
+
+        jPanel2.add(ScrollQuery1, java.awt.BorderLayout.CENTER);
+
+        jSplitPane1.setRightComponent(jPanel2);
+
+        Req1.add(jSplitPane1, java.awt.BorderLayout.CENTER);
+
+        OptionsTab.addTab("Requerimiento 1", Req1);
 
         javax.swing.GroupLayout Req2Layout = new javax.swing.GroupLayout(Req2);
         Req2.setLayout(Req2Layout);
         Req2Layout.setHorizontalGroup(
             Req2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 635, Short.MAX_VALUE)
+            .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 656, Short.MAX_VALUE)
         );
         Req2Layout.setVerticalGroup(
             Req2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 185, Short.MAX_VALUE)
+            .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 187, Short.MAX_VALUE)
         );
 
-        OptionsTab.addTab("Requerimiento 3", Req2);
+        OptionsTab.addTab("Requerimiento 2", Req2);
+
+        Req3.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
+        Req3.setAlignmentX(0.2F);
+        Req3.setAlignmentY(0.2F);
+        Req3.setPreferredSize(new java.awt.Dimension(500, 180));
+
+        javax.swing.GroupLayout Req3Layout = new javax.swing.GroupLayout(Req3);
+        Req3.setLayout(Req3Layout);
+        Req3Layout.setHorizontalGroup(
+            Req3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jScrollPane3, javax.swing.GroupLayout.DEFAULT_SIZE, 654, Short.MAX_VALUE)
+        );
+        Req3Layout.setVerticalGroup(
+            Req3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jScrollPane3, javax.swing.GroupLayout.DEFAULT_SIZE, 185, Short.MAX_VALUE)
+        );
+
+        OptionsTab.addTab("Requerimiento 3", Req3);
 
         QueryOptions.setFont(new java.awt.Font("Ubuntu", 1, 15)); // NOI18N
         QueryOptions.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Proyecto", "Proveedores", "Cargo" }));
@@ -156,13 +269,13 @@ public class GUIConstruction extends javax.swing.JFrame {
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(QueryOptions, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(SearchButton))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 15, Short.MAX_VALUE)))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 29, Short.MAX_VALUE)))
                 .addComponent(OptionsTab, javax.swing.GroupLayout.PREFERRED_SIZE, 225, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButton2)
                     .addComponent(jButton3))
-                .addContainerGap(16, Short.MAX_VALUE))
+                .addContainerGap(29, Short.MAX_VALUE))
         );
 
         OptionsTab.getAccessibleContext().setAccessibleName("Requerimiento 1");
@@ -192,8 +305,6 @@ public class GUIConstruction extends javax.swing.JFrame {
                 OptionsTab.setSelectedIndex(0);
                 break;
         }
-        
-        jTextField1.setText(selectedOption());
     }//GEN-LAST:event_SearchButtonMouseClicked
 
     
@@ -239,11 +350,17 @@ public class GUIConstruction extends javax.swing.JFrame {
     private javax.swing.JPanel Req1;
     private javax.swing.JPanel Req2;
     private javax.swing.JPanel Req3;
+    private javax.swing.JScrollPane ScrollQuery1;
     private javax.swing.JButton SearchButton;
     private javax.swing.JSeparator Separator;
+    private javax.swing.JTable TableQuery1;
+    private javax.swing.JLabel Title;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
-    private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTextField jTextField1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JSplitPane jSplitPane1;
     // End of variables declaration//GEN-END:variables
 }
